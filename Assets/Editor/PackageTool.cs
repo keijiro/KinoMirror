35eb9d4441ebeed175b122baa89b7fda2cdc042e
Keijiro Takahashi <keijiro@ZX.local>
2015-08-20 21:14:07 +0900

    Changed package base directory to avoid GUID conflict.

--- Assets/Editor/PackageTool.cs
+++ Assets/Editor/PackageTool.cs
@@ -6,6 +6,6 @@ public class PackageTool
     [MenuItem("Package/Update Package")]
     static void UpdatePackage()
     {
-        AssetDatabase.ExportPackage("Assets/Kino/Mirror", "KinoMirror.unitypackage", ExportPackageOptions.Recurse);
+        AssetDatabase.ExportPackage("Assets/Kino", "KinoMirror.unitypackage", ExportPackageOptions.Recurse);
     }
 }
